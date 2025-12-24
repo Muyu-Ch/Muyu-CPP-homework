bd0f25f426fd2b2eeac4cbb6086b8dc3f0c73dec
Muyu-Ch <1744391048@qq.com>
2025-12-24 11:01:14 +0800

    Refactor quick sort algorithm in 10-2.cpp
    Refactor quick sort implementation to improve performance and readability.

--- 10-2.cpp
+++ 10-2.cpp
@@ -1,56 +1,49 @@
 //有一个含n（n<=200000）个整数的无序序列，设计一个算法利用快速排序思路求前10个最大的元素。(快速排序)
 
+
 #include<iostream>
 #include<vector>
-#include<fstream>
 using namespace std;
 
 int partition(vector<int>& nums, int left, int right) {
-    int pivot = nums[left];
-    int i = left, j = right;
-    while (i < j) {
-        while (i < j && nums[j] <= pivot) {
-            j--;
-        }
-        nums[i] = nums[j];
-        while (i < j && nums[i] >= pivot) {
-            i++;
-        }
-        nums[j] = nums[i];
-    }
-    nums[i] = pivot;
-    return i;
+	int i = left;
+	int j = right;
+	int point = nums[left];
+
+	while (i < j) {
+		while (i<j && nums[j]>point) {
+			j--;
+		}
+		nums[i] = nums[j];
+		while (i < j && nums[i] <= point) {
+			i++;
+		}
+		nums[j] = nums[i];
+	}
+	nums[i] = point;
+	return i;
 }
 
 void quickSort(vector<int>& nums, int left, int right) {
-    if (left >= right) {
-        return;
-    }
-    int pivotIdx = partition(nums, left, right);
-    quickSort(nums, left, pivotIdx - 1);
-    quickSort(nums, pivotIdx + 1, right);
-}
-
-void first10(vector<int>nums, ofstream& outfile) {
-    for (int i = 0; i < 10; i++) {
-        outfile << nums[i] << " ";
-    }
+	if (left >= right) {
+		return;
+	}
+	int i = partition(nums, left, right);
+	partition(nums, left, i - 1);
+	partition(nums, i + 1, right);
 }
 
 int main() {
-    fstream infile("in.txt");
-    vector<int>nums;
-    int n;
-    while (infile>>n){
-        nums.push_back(n);
-    }
-    infile.close();
-    quickSort(nums,0,nums.size()-1);
-    
-    ofstream outfile("out.txt");
-    first10(nums, outfile);
-    outfile.close();
+	int n;
+	cin >> n;
+	vector<int> nums(n);
+	for (int i = 0; i < n; i++) {
+		cin >> nums[i];
+	}
 
-    return 0;
-
+	quickSort(nums, 0, n - 1);
+	for (int i = 0; i < n; i++) {
+		cout << nums[i]<<" ";
+	}
+	
 }
